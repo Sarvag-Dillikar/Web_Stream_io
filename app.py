09bd552338017a20ac940bdaa0dac3a5b66c1c6a
Sarvag Dillikar <73689548+Sarvag-Dillikar@users.noreply.github.com>
2023-02-27 14:43:41 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,15 @@
+import streamlit as st
+st.title('Sarvag Dillikar')
+st.header(':blue[Data Science Intern😎]')
+st.subheader(":red[Innomatics Research labs]")
+
+btn_click = st.button("To know more details about me...click here")
+if btn_click == True:
+    st.subheader("wow! I'm glad that.. you wanna know about me 😍")
+    st.caption('I am a recent B.TECH graduate and data enthusiastic who loves to explore data and do statistical problems and interested in Data Science')
+
+    st.subheader('Find my linkedin and Github profiles here 👇')
+
+
+    st.markdown('Linkedin : https://www.linkedin.com/in/sarvag-dillikar-b4ba4b201')
+    st.markdown('GitHub: https://github.com/Sarvag-Dillikar')
